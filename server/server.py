3f6b512d0fec56fb92867f9823da2e9d1d859e55
leext <leext@buaa.edu.cn>
2021-12-11 23:43:50 +0800

    save

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
-import enum
+import sys
+sys.path.append('/home/LAB/lixt/KBQA/')
 import json
 import argparse
 import os
@@ -15,10 +16,13 @@ from Bart_Program.model import BartCBR
 from Bart_Program.executor_rule import RuleExecutor
 from Bart_Program.program_utils import seq2program, get_program_seq, program2seq, get_program_labels
 
+from MyQA.train.Server import Server as BoostServer
 
 class ModelManager:
-
-    def __init__(self, qa_model_path, recall_model_path) -> None:
+    def __init__(self,
+                 qa_model_path,
+                 recall_model_path,
+                 base_model_path='') -> None:
         print('Loading QA model %s' % qa_model_path)
         self.qa_model = BartForConditionalGeneration.from_pretrained(
             qa_model_path)
@@ -26,34 +30,51 @@ class ModelManager:
         print('Loading Recall model %s' % recall_model_path)
         self.recall_model = BartCBR(recall_model_path)
         self.recall_model.eval()
+        if os.path.isdir(base_model_path):
+            print('Loading base model %s' % base_model_path)
+            self.base_model = BartForConditionalGeneration.from_pretrained(
+                base_model_path)
+            self.base_model.eval()
+        else:
+            self.base_model = None
 
-    def generate(self, input_ids):
+    def generate(self, input_ids, use_base=False):
         with torch.no_grad():
-            outputs = self.qa_model.generate(input_ids=input_ids, max_length=500)
+            if use_base and self.base_model is not None:
+                model = self.base_model
+                print('Use base')
+            else:
+                model = self.qa_model
+                print('Use exp')
+            outputs = model.generate(input_ids=input_ids, max_length=500)
+
         return outputs
 
     def sent_rep(self, input_ids):
         with torch.no_grad():
-            return self.recall_model.get_sent_rep(input_ids['input_ids'], input_ids['attention_mask'])
+            return self.recall_model.get_sent_rep(input_ids['input_ids'],
+                                                  input_ids['attention_mask'])
 
 
 class KBManager:
-
     def __init__(self, kb_path) -> None:
         self.executor = RuleExecutor(None, kb_path)
 
     def exec_program(self, program: str):
         func_list, inputs_list = seq2program(program)
-        func_list, inputs_list = self.executor.revise_program(
-            program, func_list, inputs_list)
+        print('Before revise:', program2seq(func_list, inputs_list))
+        # func_list, inputs_list = self.executor.revise_program(
+        #     program, func_list, inputs_list)
         answer = self.executor.forward(func_list,
                                        inputs_list,
                                        ignore_error=True)
+        if answer is None:
+            answer = '知识图谱中未找到相关信息'
+        print('After revise :', program2seq(func_list, inputs_list))
         return answer, (func_list, inputs_list)
 
 
 class IndexManager:
-    
     def __init__(self, questions, vectors) -> None:
         self.questions = questions
         self.vectors = vectors
@@ -63,27 +84,33 @@ class IndexManager:
         # print(index.shape)
         q = [self.questions[i] for i in index[0]]
         return q
+    
+    def insert(self, q, v):
+        self.questions.append(q)
+        self.vectors = torch.cat([self.vectors, v])
+
 
 class QADataManager:
-
     def __init__(self, raw_json_path) -> None:
         with open(raw_json_path, 'r') as f:
             data = json.load(f)
         self.q2program = {
-            item['question']: get_program_seq(item['program']) for item in data
+            item['question']: get_program_seq(item['program'])
+            for item in data
         }
 
     def find(self, text):
         return self.q2program.get(text)
 
-    def random_example_list(self):
+    def random_example_list(self, examples=None):
         """
         [{
             "q": "",
             "labels": [""]
         }]
         """
-        examples = random.sample(self.q2program.keys(), 6)
+        if examples is None:
+            examples = random.sample(self.q2program.keys(), 6)
         labels = [
             get_program_labels(seq2program(self.q2program[q])[0])
             for q in examples
@@ -150,22 +177,18 @@ def gen_graph(func_list, inputs_list):
 
 
 class ServerBackend:
-
-    def __init__(
-        self,
-        qa_model_path: str,
-        recall_model_path: str,
-        index_path: str,
-        kb_path: str,
-        raw_data_path: str,
-    ) -> None:
-        self.model_mgr = ModelManager(qa_model_path, recall_model_path)
+    def __init__(self, qa_model_path: str, recall_model_path: str,
+                 index_path: str, kb_path: str, raw_data_path: str,
+                 base_model_path: str) -> None:
+        self.model_mgr = ModelManager(qa_model_path, recall_model_path,
+                                      base_model_path)
         self.tokenizer = BartTokenizerFast.from_pretrained(qa_model_path)
         self.kb_mgr = KBManager(kb_path)
         self.data_mgr = QADataManager(raw_data_path)
         questions, rep = self._build_index(index_path)
         self.index_mgr = IndexManager(questions, rep)
-            
+        print('Loading boost')
+        self.boost_server = self
 
     def _build_index(self, index_path):
         print('Loading Index')
@@ -173,11 +196,23 @@ class ServerBackend:
         return questions, rep
 
     def insert_index(self, question: str, program: str):
-        pass
+        if question in self.data_mgr.q2program:
+            return
+        self.data_mgr.q2program[question] = program
+        input_ids = self.tokenizer.batch_encode_plus([question],
+                                                    padding=True,
+                                                    max_length=1024,
+                                                    truncation=True,
+                                                    return_tensors='pt')
+        rep = self.model_mgr.recall_model.get_sent_rep(input_ids['input_ids'],
+                                        input_ids['attention_mask'])
+        self.index_mgr.insert(question, rep)
+        print('new size %d, %d' % (len(self.data_mgr.q2program), len(self.index_mgr.vectors)))
 
-    def answer(self, question: str):
+
+    def answer(self, question: str, use_base=False):
         # p = self.data_mgr.find(question)
-        p = self.gen_program(question)
+        p = self.gen_program(question, use_base)
         ans, (func_list, inputs_list) = self.kb_mgr.exec_program(p)
         return {
             'answer': ans,
@@ -194,24 +229,29 @@ class ServerBackend:
             'graph': gen_graph(func_list, inputs_list)
         }
 
-
-    def _prepare_qa_input(self, question: str, cases: List[Tuple[str, str]]):
-        question = 'Question 0 %s Program for Question 0 is' % question
-        sents = [question]
-        for idx, (q, p) in enumerate(cases):
-            question = 'Question %d %s Program for Question %d is %s' % (
-                idx + 1, q, idx + 1, p)
-            sents.append(question)
-        sents.append('Program for Question 0 is')
-        input_str = ' '.join(sents)
+    def _prepare_qa_input(self,
+                          question: str,
+                          cases: List[Tuple[str, str]],
+                          use_base=False):
+        if not use_base:
+            question = 'Question 0 %s Program for Question 0 is' % question
+            sents = [question]
+            for idx, (q, p) in enumerate(cases):
+                question = 'Question %d %s Program for Question %d is %s' % (
+                    idx + 1, q, idx + 1, p)
+                sents.append(question)
+            sents.append('Program for Question 0 is')
+            input_str = ' '.join(sents)
+        else:
+            input_str = question
         input_ids = self.tokenizer.batch_encode_plus([input_str],
                                                      padding=True,
                                                      max_length=1024,
                                                      truncation=True,
                                                      return_tensors='pt')
         return input_ids['input_ids']
-    
-    def _prepare_recall_input(self, question:str):
+
+    def _prepare_recall_input(self, question: str):
         if type(question) is str:
             question = [question]
         input_ids = self.tokenizer.batch_encode_plus(question,
@@ -222,25 +262,31 @@ class ServerBackend:
         return input_ids
 
     def _decode(self, output_ids):
-        res = [self.tokenizer.decode(output,
-                                    skip_special_tokens=True,
-                                    clean_up_tokenization_spaces=True)
-                                    for output in output_ids]
+        res = [
+            self.tokenizer.decode(output,
+                                  skip_special_tokens=True,
+                                  clean_up_tokenization_spaces=True)
+            for output in output_ids
+        ]
         return res
 
-    def gen_program(self, question: str):
-        input_ids = self._prepare_recall_input(question)
-        q_rep = self.model_mgr.sent_rep(input_ids)
-        q_cases = self.index_mgr.get_topk(q_rep, 5)
-        cases = [(q, self.data_mgr.q2program.get(q)) for q in q_cases]
-        input_ids = self._prepare_qa_input(question, cases)
-        output_ids = self.model_mgr.generate(input_ids)
+    def gen_program(self, question: str, use_base=False):
+        if not use_base:
+            input_ids = self._prepare_recall_input(question)
+            q_rep = self.model_mgr.sent_rep(input_ids)
+            q_cases = self.index_mgr.get_topk(q_rep, 5)
+            cases = [(q, self.data_mgr.q2program.get(q)) for q in q_cases]
+        else:
+            cases = []
+        input_ids = self._prepare_qa_input(question, cases, use_base)
+        output_ids = self.model_mgr.generate(input_ids, use_base)
         program = self._decode(output_ids)[0]
         return program
 
+
 def build_index(args):
     device = torch.device(args.device)
-    with open('./dataset/train.json','r') as f:
+    with open('./dataset/train.json', 'r') as f:
         data = json.load(f)
     questions = [item['question'] for item in data]
     recall_model = BartCBR(args.recall_model_path)
@@ -248,16 +294,18 @@ def build_index(args):
     tokenizer = BartTokenizerFast.from_pretrained(args.qa_model_path)
     recall_model.to(device)
     with torch.no_grad():
-        batch_size=64
+        batch_size = 64
         reps = []
         for i in tqdm(range(0, len(questions), batch_size)):
-            input_ids = tokenizer.batch_encode_plus(questions[i:i+batch_size],
-                                                     padding=True,
-                                                     max_length=1024,
-                                                     truncation=True,
-                                                     return_tensors='pt')
+            input_ids = tokenizer.batch_encode_plus(questions[i:i +
+                                                              batch_size],
+                                                    padding=True,
+                                                    max_length=1024,
+                                                    truncation=True,
+                                                    return_tensors='pt')
             input_ids.to(device)
-            rep = recall_model.get_sent_rep(input_ids['input_ids'], input_ids['attention_mask'])
+            rep = recall_model.get_sent_rep(input_ids['input_ids'],
+                                            input_ids['attention_mask'])
             reps.append(rep.cpu())
         rep = torch.cat(reps)
         torch.save((questions, rep), args.index_path)
@@ -266,8 +314,9 @@ def build_index(args):
 def run_flask(args):
     kb_path = './dataset/kb.json'
     raw_data_path = './dataset/train.json'
-    server = ServerBackend(args.qa_model_path, args.recall_model_path, args.index_path,
-                           kb_path, raw_data_path)
+    server = ServerBackend(args.qa_model_path, args.recall_model_path,
+                           args.index_path, kb_path, raw_data_path,
+                           args.base_model_path)
     app = Flask(__name__,
                 static_folder="./dist/assets",
                 template_folder="./dist")
@@ -284,12 +333,38 @@ def run_flask(args):
 
     @app.route('/examples')
     def get_examples():
-        return jsonify(server.data_mgr.random_example_list())
+        model_name = request.args['model']
+        if model_name == '数据增强方法':
+            jsonify(server.boost_server.random_example_list())
+        else:
+            return jsonify(server.data_mgr.random_example_list())
+
+    @app.route('/init_examples')
+    def get_init_examples():
+        model_name = request.args['model']
+        init_q = [
+            "Was Shelley Duvalls, a cast member of The Shining, playing Wendy Torrance, born before 1984?",
+            "Which film has the shorter runtime, The Avengers (in the spy film genre) or Quadrophenia (released in Denmark)?",
+            "How many London areas are in the United Kingdom or correspond to local dialing code 020?",
+            "How many miniseries that were published before 1984 mainly depicted World War II?",
+            "Does Palo Alto, birthplace of Hewlett-Packard, or Charleston, capital of West Virginia, have less elevation above sea level?",
+            "Among the feature films that is distributed at United Kingdom,which one has the smallest duration ?"
+        ]
+        if model_name == '数据增强方法':
+            jsonify(server.boost_server.random_example_list())
+        return jsonify(server.data_mgr.random_example_list(init_q))
 
     @app.route('/qa')
     def qa():
         q = request.args['question']
-        res = server.answer(q)
+        model_name = request.args['model']
+        method_name = request.args['method']
+        use_base = method_name == '基线'
+        print('Question:', q)
+        if model_name == '数据增强方法':
+            res = server.boost_server.answer(q)
+        else:
+            res = server.answer(q, use_base)
         return res
 
     @app.route('/exec')
@@ -302,18 +377,22 @@ def run_flask(args):
     def save_case():
         p = request.args['program']
         q = request.args['question']
+        server.insert_index(q, p) 
         return "ok"
 
     app.run('0.0.0.0', args.port, debug=True)
 
 
-best_qa_model = './Bart_Program/saves/mtl_rel_cbr_infill+2+_kbp2_0.5_w0.1_real_smp_1.0_revise/epoch_52'
+# best_qa_model = './Bart_Program/saves/mtl_rel_cbr_infill+2+_kbp2_0.5_w0.1_real_smp_1.0_revise/epoch_52'
+best_qa_model = './Bart_Program/saves/mtl_qcbr_rel_0.1_kbp_0.5_real_smp_1.0_revise/epoch_25'
+base_qa_model = './Bart_Program/saves/vanilla_0.1/epoch_25'
 best_recall_model = './Bart_Program/saves/recall_rule_rel/epoch_3'
 index_path = './server/index/index.pt'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', default=9997, type=int)
     parser.add_argument('--qa_model_path', default=best_qa_model)
+    parser.add_argument('--base_model_path', default=base_qa_model)
     parser.add_argument('--recall_model_path', default=best_recall_model)
     parser.add_argument('--index_path', default=index_path)
     parser.add_argument('--device', default='cpu')
